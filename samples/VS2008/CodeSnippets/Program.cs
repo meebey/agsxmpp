0f6fcef1e86d6925d0961aa4cd790df7d8f64f4c
alex <alex@75836ab5-d07f-044f-868d-97678344ed03>
2007-12-07 10:52:32 +0000

    initial checkin of CodeSnippets
    git-svn-id: svn://svn.ag-software.de/agsxmpp/trunk@26 75836ab5-d07f-044f-868d-97678344ed03

---
+++ samples/VS2008/CodeSnippets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+using System.Xml;
+
+using agsXMPP.Xml.Dom;
+
+namespace CodeSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shim shim = new Shim();
+
+            Presence pres = new Presence();
+
+            Console.WriteLine("Press enter key to close this program!");
+            Console.ReadLine();
+        }
+
+        internal static void Print(Element el)
+        {
+            Console.WriteLine(el.ToString(Formatting.Indented));
+            Console.WriteLine("\r\n");
+        }
+    }
+}
