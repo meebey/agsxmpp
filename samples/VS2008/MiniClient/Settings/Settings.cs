39e1fe44f58fc33a91000945287c66e67fd0f094
alex <alex@75836ab5-d07f-044f-868d-97678344ed03>
2008-03-02 22:04:42 +0000

    git-svn-id: svn://svn.ag-software.de/agsxmpp/trunk@57 75836ab5-d07f-044f-868d-97678344ed03

--- samples/VS2008/MiniClient/Settings/Settings.cs
+++ samples/VS2008/MiniClient/Settings/Settings.cs
@@ -5,22 +5,9 @@ using agsXMPP.Xml.Dom;
 
 namespace MiniClient.Settings
 {
-    /*
-    <Settings>
-       <Login>
-          <Jid>gnauck@jabber.org</Jid>
-          <Password>0610</Password>
-          <Resource>MiniClient</Resource>
-          <Priority>10</Priority>
-          <Port>5222</Port>
-          <Ssl>false</Ssl>
-       </Login>
-    </Settings>
-     * 
-    
+    /*   
     This class shows how agsXMPP could also be used read and write custom xml files.
-    Here we use it for the application settings which are stored in xml files
-    
+    Here we use it for the application settings which are stored in xml files    
     */
     public class Settings : Element
     {
