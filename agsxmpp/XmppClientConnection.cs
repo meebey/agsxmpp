dcaef518829b79af20304e1716b959e29ef68a04
alex <alex@75836ab5-d07f-044f-868d-97678344ed03>
2010-09-13 18:23:27 +0000

    git-svn-id: svn://svn.ag-software.de/agsxmpp/trunk@111 75836ab5-d07f-044f-868d-97678344ed03

--- agsxmpp/XmppClientConnection.cs
+++ agsxmpp/XmppClientConnection.cs
@@ -234,10 +234,10 @@ namespace agsXMPP
 			get { return m_Priority; }
 			set
             {
-                if (value < -127 || value > 127)
-                    m_Priority = value;
-                else
+                if ((value < -127) || (value > 127))
                     throw new ArgumentException("The value MUST be an integer between -128 and +127");
+                
+                m_Priority = value;
 			}
 		}
 
